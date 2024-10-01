36189df1f024ce81cc20b4831ad5f27323b3f724
Dipesh Arwat <dipesharwat80@gmail.com>
2024-10-01 17:15:35 +0530

    add links

--- todo-list/src/components/Auth/Signup.js
+++ todo-list/src/components/Auth/Signup.js
@@ -31,7 +31,7 @@ const Signup = () => {
         if (!validate()) return;
 
         try {
-            await axios.post('https://todolist-app-mern-backend.onrender.com/api/auth/register', { name, email, password });
+            await axios.post('http://localhost:5000/api/auth/register', { name, email, password });
             navigate('/login');
         } catch (error) {
             console.error('Error signing up', error);
